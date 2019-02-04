d5490d19c21c207041127bd3955ae359e3bbc617
SolidRocker <kamtk.tyler@gmail.com>
2019-02-04 17:03:32 +0800

    Updates

---
+++ precache-manifest.4a1a9896b388430ac49cce05a234b98f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c06708ef311fc33da4fe",
+    "url": "/isitopen/static/js/runtime~main.c06708ef.js"
+  },
+  {
+    "revision": "33e4369a7e2f3588a3e4",
+    "url": "/isitopen/static/js/main.33e4369a.chunk.js"
+  },
+  {
+    "revision": "6f8d4fa47950314cbeb5",
+    "url": "/isitopen/static/js/1.6f8d4fa4.chunk.js"
+  },
+  {
+    "revision": "33e4369a7e2f3588a3e4",
+    "url": "/isitopen/static/css/main.6ea39fd2.chunk.css"
+  },
+  {
+    "revision": "ea5647f6ad5675a1419552031e5c362f",
+    "url": "/isitopen/index.html"
+  }
+];
